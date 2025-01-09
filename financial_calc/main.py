5b8a7b7effd0f6ce855afdb7aa2a2a8adc92d07e
scarggy-design1 <lizzy.saldana@ucas-edu.net>
2025-01-09 13:44:12 -0700

    goo

---
+++ financial_calc/main.py
@@ -0,0 +1,22 @@
+#Nicoles financial calc
+
+def compound():
+    money = float(input("How much money do you have in your compound interest account?: "))
+    percent = float(input("What is the percentage of your compound interest?: "))
+    c
+
+def budget():
+    income = float(input("What is your income?: "))
+    savings = income*0.2
+    entertainment = income*0.2
+    food = income*0.3
+    home = income*0.3
+    print("according to this, your income of", income, " dollars per year, you should budget your money like so: ")
+    print("savings: " ,savings,
+          "entertainment: ", entertainment,
+           "food: ", food,
+            "home: ", home)
+
+
+def main():
+    pass
