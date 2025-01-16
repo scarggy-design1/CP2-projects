e1ac3b60f66754cc4888dfd8a8918c758a2fb68c
scarggy-design1 <lizzy.saldana@ucas-edu.net>
2025-01-16 13:37:05 -0700

    library

---
+++ Personal_library/main.py
@@ -0,0 +1,6 @@
+#Nicole's Personal Library
+
+music = {("Dance Away", "Roxette"), ("Play That Funky Music", "Wild Cherry"), ("I've Had Enough(Into the Fire)", "KISS"),
+         ("Zanzibar", "Billy Joel"), ("Physical Fasination", "Roxette"), ("The Look", "Roxette")}
+    
+
