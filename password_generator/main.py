7a578c9caebd4df2b01ff0e2511bcaf99f6527ac
scarggy-design1 <lizzy.saldana@ucas-edu.net>
2025-01-23 13:22:08 -0700

    help

---
+++ password_generator/main.py
@@ -0,0 +1,16 @@
+#This is Nicoles password generator
+
+import string
+
+import random
+
+print(string.ascii_lowercase)
+print(string.ascii_uppercase)
+print(string.ascii_letters)
+print(string.digits)
+
+def randomizer():
+    print("Welcome to random password generator! This program will print 4 passwords that meet your criteria. ")
+    criteria = input("""What would you like 
+
+""")
