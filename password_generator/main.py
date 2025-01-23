59253ae0e49be8ab1abe7f017f8b76f3b68daee3
scarggy-design1 <lizzy.saldana@ucas-edu.net>
2025-01-23 13:41:57 -0700

    got start

--- password_generator/main.py
+++ password_generator/main.py
@@ -2,15 +2,40 @@
 
 import string
 
-import random
-
-print(string.ascii_lowercase)
-print(string.ascii_uppercase)
-print(string.ascii_letters)
-print(string.digits)
 
 def randomizer():
     print("Welcome to random password generator! This program will print 4 passwords that meet your criteria. ")
-    criteria = input("""What would you like 
+    criteria = input("""What is your criteria?:
+                     REQUIRED: has to have at least 1 special charcater and 1 number.
+                     1. include specifics special characters (you choose)
+                     2. Include specific numbers (you choose)
+                     3. Include specific words/phrases (you choose)
+                     4. exit
+                     \n""")
+    if criteria == '1':
+        a2 = string.punctuation
+        a = input(f"You can pick any specical characters! They include: {a2}\n")
+        required_specials = []
+        required_specials.append(a)
+        for a in string.punctuation:
+            print("You chose:\n", required_specials)
+        else:
+            print("Nice try, actually choose a required SPECIAL character(s)...")
+    elif criteria == '2':
+        b2 = string.digits
+        b = input(f"You can pick any digits! They include: {b2}\n")
+        required_digits = []
+        required_digits.append(b)
+        print("You chose:\n", required_digits)
+    elif criteria == '3':
+        c2 = string.digits
+        c = input(f"You can pick any words/phrases! They include: {c2}\n")
+        required_phrase = []
+        required_phrase.append(c)
+        print("You chose:\n", required_phrase)
+    elif criteria == '4':
+        pass
+    else:
+        print("not a valid choice. Pick again\n")
 
-""")
+randomizer()
