3708e80bfc72cde69ada06f3754aefd5005b3434
scarggy-design1 <lizzy.saldana@ucas-edu.net>
2025-01-27 13:43:25 -0700

    help

--- password_generator/main.py
+++ password_generator/main.py
@@ -1,44 +1,40 @@
 #This is Nicoles password generator
 
 import string
-
+import random
 
 def randomizer():
-    print("Welcome to random password generator! This program will print 4 passwords that meet your criteria. ")
-    criteria = input("""What is your criteria?:
-                     REQUIRED: has to have at least 1 special charcater and 1 number.
-                     1. include specifics special characters (you choose)
-                     2. Include specific numbers (you choose)
-                     3. Include specific words/phrases (you choose)
-                     4. exit
-                     \n""")
-    if criteria == '1':
-        a2 = string.punctuation
-        a = input(f"You can pick any specical characters! They include: {a2}\n")
-        required_specials = []
-        required_specials.append(a)
-        print("You chose:\n", required_specials)
+    basics = str(string.ascii_letters)
+    basics2 = str(string.digits)
+    basics3 = str(string.punctuation)
+    password = list(basics)
     
-    elif criteria == '2':
-        b2 = string.digits
-        b = input(f"You can pick any digits! They include: {b2}\n")
-        required_digits = []
-        required_digits.append(b)
-        print("You chose:\n", required_digits)
-    elif criteria == '3':
-        c2 = string.digits
-        c = input(f"You can pick any words/phrases! They include: {c2}\n")
-        required_phrase = []
-        required_phrase.append(c)
-        print("You chose:\n", required_phrase)
-    elif criteria == '4':
-        return 'exit'
+    print(password)
+
+    num = int(input("How long do you want your password to be?(HAS to be greater than 8) "))
+    try:
+        print(num)
+    except ValueError:
+        print("Choose a valid NUMBER.")
+        return 'error'
+    if num < 8:
+        print("Dude, seriously choose a number higher than 8")
+        return 'error'
+    for i in range(1,5):
+        random.shuffle(password)
+        print(password[1:num])
+
+
+
+
+def main():
+    print("Welcome to random password generator!")
+    the_random = randomizer()
+    if the_random == 'error':
+        randomizer()
     else:
-        print("not a valid choice. Pick again\n")
-
-randomizer()
+        pass
 
 
-"LaRose, Miller, K"
 
-"Blunt, Shurtz, Childress, Lamb,"
+main()
