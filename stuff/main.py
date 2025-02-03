88fd8aafc52222eb1eb905d18bfac6cfdbccbffd
scarggy-design1 <lizzy.saldana@ucas-edu.net>
2025-02-03 13:27:27 -0700

    hello?

---
+++ stuff/main.py
@@ -0,0 +1,28 @@
+#Nicoles stuff, notes about dictionaries
+
+car = {
+    "Make": "Ford",
+    "Model": "Escape xlt",
+    "Year": 2008,
+    "Color": "Red"
+}
+
+print(car["Make"][2])
+
+
+students = {
+    "sixth": {
+        1:"Nicole", 
+        2:"Luke", 
+        3: "Gavin",
+        4: "Jackson"
+
+    },
+    "First": {
+        1: "Hello",
+        2: "Evan",
+        3: "HE"
+    }
+}
+
+print(students["sixth"][2])
