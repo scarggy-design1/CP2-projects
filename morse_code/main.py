a00afe60b4834000e29010e37c141645002c837a
scarggy-design1 <lizzy.saldana@ucas-edu.net>
2025-02-03 13:43:35 -0700

    HELP

---
+++ morse_code/main.py
@@ -0,0 +1,22 @@
+#This is Nicoles Morse Code
+import string
+uppers = string.ascii_uppercase
+LETTERS = list(uppers)
+
+MORSE_CODES=[' .- ',' -... ',' -.-. ',' -.. ',' . ',' ..-. ',' --. ',' .... ',' .. ',
+' .--- ',' -.- ',' .-.. ',' -- ',' -. ',' --- ',' .--. ',' --.- ',' .-. ',
+' ... ',' - ',' ..- ',' ...- ',' .-- ',' -..- ',' -.-- ',' --.. ']
+
+def convert_EM():
+    pass
+
+def convert_ME():
+    pass
+
+def main():
+    ask = input("""
+What would you like to do?
+1. Convert to English
+2. Convert to Morse
+                """)
+
