f7b074e0c346c26608a91e284925f45ce036c218
scarggy-design1 <lizzy.saldana@ucas-edu.net>
2025-02-14 13:40:55 -0700

    interesting

--- stuff/main.py
+++ stuff/main.py
@@ -1,41 +1,17 @@
-"""#Nicoles stuff, notes about dictionaries
+import os
+from time import sleep
+def cs(): # Clear Screen
+    print("\033c",end="")
+# some text
+print("a")
+print("b")
+print("c")
+print("d")
+print("e")
+print("Screen will now be cleared in 5 Seconds")
 
-car = {
-    "Make": "Ford",
-    "Model": "Escape xlt",
-    "Year": 2008,
-    "Color": "Red"
-}
+# Waiting for 5 seconds to clear the screen
+sleep(2)
 
-print(car["Make"])
-
-
-students = {
-    "sixth": {
-        1:"Nicole", 
-        2:"Luke", 
-        3: "Gavin",
-        4: "Jackson"
-
-    },
-    "First": {
-        1: "Hello",
-        2: "Evan",
-        3: "HE"
-    }
-}"""
-
-d = {'a': 1, 'b': 2, 'c': 3}
-
-d.update({'d': 4, 'e': 5})
-print(d)
-
-
-d = {'a': 1, 'b': 2, 'c': 3}
-
-d.setdefault('d', 4)
-d.setdefault('e', 5)
-print(d)
-
-#d.setdefault('d', 4) checks if the key 'd' exists and if it does, it returns the existing value. Otherwise, it adds 'd': 4.
-#d.setdefault('e', 5) does the same for the key 'e', adding 'e': 5 since it doesn't exist.
+# Clearing the Screen
+cs()
