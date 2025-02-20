065b3460874fd3931ccf937abcec16abc5a2c620
scarggy-design1 <lizzy.saldana@ucas-edu.net>
2025-02-20 13:27:28 -0700

    well oh gee what the heck man

---
+++ ToDo/main.py
@@ -0,0 +1,31 @@
+#This is Nicoles to-do list
+
+to_do_list = []
+with open("ToDo/to_do.txt", "r") as file: 
+    reader = 
+    for row in reader:
+        to_do_list.append(row)
+
+
+
+
+def entire_list(): 
+    print("_____________________________________________\n")
+    for to_do in to_do_list:
+        print(f"- {to_do['Assignment']}\n")
+
+
+entire_list()
+
+
+def add():
+    add_to
+
+def complete():
+    pass
+
+def delete():
+    pass
+
+
+
