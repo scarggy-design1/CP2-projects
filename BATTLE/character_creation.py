86b5acbfeff0aa4fe680f0d2c2fb5c6d296c652d
scarggy-design1 <lizzy.saldana@ucas-edu.net>
2025-03-07 13:43:38 -0700

    why are you real

--- BATTLE/character_creation.py
+++ BATTLE/character_creation.py
@@ -1,21 +1,40 @@
 import random
+import csv
+
+def checker(x):
+    if x == 0:
+        pass
+    elif x > 20:
+        return 'negative'
+    elif x < 0:
+        return 'over'
+    else:
+        pass
 
 def create():
     name = input("Welcome to CREATION...\nWhat do you want to name your character?: ")
     try:
         points = 20
-        speed = int(input("What is the speed of your character? "))
-        points-speed
-        health = int(input("What is the health of your character? "))
-        points-health
-        brawn = int(input("What is the strength of your character? "))
-        points-brawn
-        defense = int(input("What is the defense of your character? "))
-        points-defense
+        speed = int(input(f"What is the speed of your character? You have {points} points left\n"))
+        points-=speed
+        checker(points)
+        health = int(input(f"What is the health of your character? You have {points} points left\n"))
+        points-=health
+        checker(points)
+        brawn = int(input(f"What is the strength of your character? You have {points} points left\n"))
+        points-=brawn
+        checker(points)
+        defense = int(input(f"What is the defense of your character? You have {points} points left\n"))
+        points-=defense
+        checker(points)
         if points != 0:
-            continue
+            print("Please make sure you pick numbers that add up to 20 points!")
         else:
             pass
     except:
         print('DUDE.\nOkay freak actually pick a number thats valid.\n')
-        continue
+        
+
+def characters():
+    pass
+
--- BATTLE/main.py
+++ BATTLE/main.py
@@ -1,13 +1,17 @@
 
-from character_creation import create as creation
+from character_creation import create as creation, characters as character
 
 def main():
     while True:
-        ask = input("WELCOME TO\n-- THE GAME --\nBefore you play, what would you like to do?\n1) CREATE character\n2) Pick precreated character\n3) EXIT").strip()
+        ask = input("WELCOME TO (drum roll please)\n-- THE GAME --\nBefore you play, what would you like to do?\n1) CREATE character\n2) PICK precreated character\n3) EXIT").strip()
         if ask == '1':
             creation()
         elif ask == '2':
-            pass
+            character()
         elif ask == '3':
             break
+        else:
+            print("INVALID\nChoose a VALID answer dood.")
         
+
+main()
