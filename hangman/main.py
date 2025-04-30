fffaa2302f3538f7a502f056ef4b55a13cdb4c3a
scarggy-design1 <lizzy.saldana@ucas-edu.net>
2025-04-30 14:12:19 -0600

    HELP ME

---
+++ hangman/main.py
@@ -0,0 +1,14 @@
+#import faker
+
+#Faker generate a random word, set this to a variable
+
+def word(rand_word):
+    wordy = list(rand_word)
+    num = len(wordy)
+    guess = 
+
+
+
+def guesser(rand_word):
+
+
